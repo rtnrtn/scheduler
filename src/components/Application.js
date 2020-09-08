6f5d460270de06e1ad2f51471348fbad397847de
Tania Nardandrea-Isakovic <rtnrtn@gmail.com>
2020-09-08 00:33:37 +0000

    Testing with mock API responses.

--- src/components/Application.js
+++ src/components/Application.js
@@ -57,10 +57,10 @@ export default function Application(props) {
       <section className="schedule">
         <section className="schedule">
           {appointments}
-          <Appointment 
+          {appointments.length > 0 && <Appointment 
             key="last" 
             time="5pm" 
-          />
+          />}
         </section>
       </section>
     </main>
